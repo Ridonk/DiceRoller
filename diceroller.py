17fa919198867fe3e89898318de9df5cf0ff80a7
Ridonk <georgeddunbar@gmail.com>
2018-12-22 21:28:38 -0500

    Refactored for OOP approach. Additionally, added summary method to count total rolls and simplify war gaming usage (especially with large numbers of dice)

--- diceroller.py
+++ diceroller.py
@@ -3,42 +3,71 @@
 import random
 
 
-# Roll n dice with x sides and return a list of the results
-def roll(numberofdice, numberofsides):
-    results = []
-    for i in range(0, numberofdice):
-        results.append(random.randint(1, numberofsides))
-        i += 1
-    results.sort()
-    return results
+class Dice:
+
+    def __init__(self, sides):
+        self.sides = sides
+        self.results = []
+        self.total_count = {}
+
+    def _roll(self):
+        return random.randint(1, self.sides)
+
+    def roll(self, number_of_dice=1):
+        for i in range(0, number_of_dice):
+            self.results.append(self._roll())
+
+    def print_die_results(self):
+        for result in self.results:
+            print(str(result))
+        print(self.count_results())
+        return
+
+    def count_results(self):
+        self.results.sort()
+        for result in self.results:
+            if result in self.total_count:
+                self.total_count[result] += 1
+            else:
+                self.total_count[result] = 1
+        return self.total_count
 
 
-# Print each result in a list
-def print_roll(roll_list):
-    for result in roll_list:
-        print(result)
+def get_number_input():
+    while True:
+        try:
+            user_input = int(input(">>> "))
+            return int(user_input)
+        except ValueError:
+            print("Invalid input. Try again with numbers.")
+
+
+def main_menu():
+    while True:
+        print('''
+        ==========
+        MAIN MENU
+        ==========
+
+        1. Roll Dice
+        2. Quit
+
+        ''')
+        user_input = get_number_input()
+        if user_input == 1:
+            print("How many dice? ")
+            number_of_dice = get_number_input()
+            print("How many sides per die? ")
+            number_of_sides = get_number_input()
+            print("You entered {} dice with {} sides.".format(number_of_dice, number_of_sides))
+            dice = Dice(number_of_sides)
+            dice.roll(number_of_dice)
+            dice.print_die_results()
+        elif user_input == 2:
+            quit(0)
 
 
 print("Hello! Welcome to DiceRoller.")
 
-# Groundwork for a more adaptive menu
-running = True
-
 # Main Loop
-while running:
-    # Store the number of dice to roll
-    ndice = int(input("Please enter how many dice: "))
-    # Store how many sides each die has
-    xsides = int(input("Please  enter how many sides on each die: "))
-    # Tell the user what we're processing
-    print("You entered " + str(ndice) + " dice and " + str(xsides) + " sides per die.")
-    # Sanity check before bothering to generate random numbers.
-    if ndice > 0 and xsides > 0:
-        print_roll(roll(ndice, xsides))
-    else:
-        print("Invalid roll attempted. The FBI has been notified.")
-    check_quit = input("To quit enter q or just press enter to continue: ")
-    if check_quit != 'q':
-        running = True
-    else:
-        running = False
+main_menu()
